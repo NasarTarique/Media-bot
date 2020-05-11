71325fc076fbcfb5ad58823298257d7378c160e9
NasarTarique <nasartarique@gmail.com>
2020-05-11 22:55:54 +0530

    daily news

--- flskapp.py
+++ flskapp.py
@@ -11,6 +11,7 @@ update_queue = Queue()
 bot = Bot(TOKEN)
 updater = Updater(TOKEN, use_context=True)
 j = updater.job_queue
+listofjobs =[]
 
 
 def start(update: Update, context: CallbackContext):
@@ -22,7 +23,6 @@ def start(update: Update, context: CallbackContext):
                               updates only once every 24 hours . 
                               type /help to know more ''')
 
-
 def help(update: Update, context: CallbackContext):
     txt = '''(Note : you will get posts from insta ,twitter and youtube only from the users you have selected not the 
     ones you follow or are subscribed to) \n commands:\n /startAll : Get updates of last 24 hrs from insta, twitter , 
@@ -33,7 +33,7 @@ def help(update: Update, context: CallbackContext):
     ]: Add the person whose insta post updates you want \n /AddTweetUser [username ] : Add the twitter handle whose 
     tweets you want to see  \n /AddYtSub [channel_name]: Add the youtube channel name whose vid updates you want\n 
     /RemoveTweetUser [username ]: stop the tweet update from this user \n /RemoveInstaUser [username] : stop post 
-    update from this user \n /RemoveYtSub [username] : stop video updates from this channel /stopALL : stop the daily 
+    update from this user \n /RemoveYtSub [username] : stop video updates from this channel\n /stopALL : stop the daily 
     updates \n /stopInsta : stops instagram updates\n /stopTweet : stop twitter updates \n /stopYt : stop youtube 
     updates \n /stopNews :stop news updates '''
     context.bot.send_message(chat_id=update.message.chat.id, text=txt)
@@ -41,6 +41,7 @@ def help(update: Update, context: CallbackContext):
 
 def callback_news(context: CallbackContext):
     txt = news.get_news()
+    listofjobs.append(context.job)
     context.bot.send_message(chat_id=context.job.context, text=txt)
 
 
@@ -51,21 +52,23 @@ def start_news(update: Update, context: CallbackContext):
 
 
 def stop_news(update: Update, context: CallbackContext):
-    if 'job' not in context.chat_data:
-        update.message.reply_text("you are not subscribed to daily news")
-
-    job = context.chat_data['job']
+    job=listofjobs[0]
+    job.schedule_removal()
+    update.message.reply_text("The daily update has been scheduled for removal")
+    '''
+    print('job')
     job.schedule_removal()
     del context.chat_data['job']
 
     update.message.reply_text("The daily update has been scheduled for removal")
+    '''
 
 
 dispatcher = Dispatcher(bot, update_queue, job_queue=j, use_context=True)
 dispatcher.add_handler(CommandHandler('help', help, pass_update_queue=True))
 dispatcher.add_handler(CommandHandler('start', start, pass_update_queue=True))
 dispatcher.add_handler(CommandHandler('startNews', start_news, pass_job_queue=True))
-dispatcher.add_handler(CommandHandler('stopNews', stop_news, pass_job_queue=True))
+dispatcher.add_handler(CommandHandler('stopNews', stop_news, pass_job_queue=True,pass_chat_data=True))
 j.set_dispatcher(dispatcher)
 thread = Thread(target=dispatcher.start, name='dispatcher')
 thread.start()
@@ -89,3 +92,5 @@ if __name__ == '__main__':
     app.run(debug=True)
     j.start()
     dispatcher.start()
+
+
--- news.py
+++ news.py
@@ -5,9 +5,9 @@ from config import NEWS_API_KEY
 def get_news():
     newsapi = NewsApiClient(api_key=NEWS_API_KEY)
     headlines = newsapi.get_top_headlines(country='in')
-    str ='News:\n'
-    for x in range(11):
-        str = str+headlines['articles'][x]['title']+'\n'+headlines['articles'][x]['description']+'\n'+headlines['articles'][x]['url']+'\n\n'
-    return str
+    strn ='News:\n'
+    for x in range(9):
+        strn = strn+'\n'+headlines['articles'][x]['title']+'\n'+headlines['articles'][x]['description']+'\n'+headlines['articles'][x]['url']+'\n\n '
+    return strn
 
 
